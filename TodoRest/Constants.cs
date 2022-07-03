233255b5858a6e30722ef748c23d4c31768c5701
daniel3scripts <paul.lugo@tecsup.edu.pe>
2022-07-02 23:55:22 -0500

    new commit

---
+++ TodoRest/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoRest
+{
+    public static class Constants
+    {
+        public static string RestUrl = "http://18.232.226.120/TodoAPI/api/todoitems/{0}";
+    }
+}
---
+++ TodoRest/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TodoRest.Model;
+
+namespace TodoRest.Data
+{
+    public interface IRestService
+    {
+        Task<List<TodItem>> RefreshDataAsync();
+
+        Task SaveTodoItemAsync(TodItem item,bool isNewItem);
+        Task DeleteTodoItemAsync(string id);
+    }
+}
